6ccf63939f964efa0655ff492b7c6a52eb49e892
catmorgan72 <113738039+catmorgan72@users.noreply.github.com>
2022-11-18 18:40:50 -0500

    showing current temp/description/humidity/wind.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
 
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+}
+
+let apiKey = "e63e6060bf09d0o6a94a3647te5cbfb5";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=pensacola&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
